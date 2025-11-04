cfb981fc7d066679f0ca796fcaba6fac16f82997
Iurii Melnyk <kn24-yu.melnyk@nubip.edu.ua>
2025-11-04 09:28:47 +0200

    Lab4 tasks completed

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,11 +1,20 @@
-// Задача: Написати функцію, яка приймає рядок і замінює всі голосні (a, e, i, o, u, y) 
-// на певний символ, наприклад *.
-
 function replaceVowels(str) {
-  // Ваш код тут
+  const vowels = "aeiouyAEIOUY";
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (vowels.includes(char)) {
+      result += "*";
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
 }
 
-console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
-console.log(replaceVowels("Javascript"));  // Виведе: "J*v*scr*pt"
+console.log(replaceVowels("hello world"));
+console.log(replaceVowels("Javascript"));
 
 module.exports = replaceVowels;
